4a84cbf70f0a893b2fa47b850e1e64ee1d6b7927
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-03 15:39:52 -0400

    Updates

---
+++ precache-manifest.1eb5e56b4042a72510d47a2cba65845f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91b3d4b98d6153a4500f4c7a391eb06",
+    "url": "/npm-package-download-count/index.html"
+  },
+  {
+    "revision": "d0b1b3b35f2461235ca7",
+    "url": "/npm-package-download-count/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c8192d875eae700d39de",
+    "url": "/npm-package-download-count/static/js/2.dd9936ef.chunk.js"
+  },
+  {
+    "revision": "d0b1b3b35f2461235ca7",
+    "url": "/npm-package-download-count/static/js/main.cdc7edf5.chunk.js"
+  },
+  {
+    "revision": "bb5824ab8cc968aa3bf9",
+    "url": "/npm-package-download-count/static/js/runtime~main.75c48a53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npm-package-download-count/static/media/logo.5d5d9eef.svg"
+  }
+]);
