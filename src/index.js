adc53b2cb1413d895a297868b10e685d2ce07c4b
PurnimaNaik <naikpurnima9@gmail.com>
2020-01-22 17:34:49 -0500

    CU

--- src/index.js
+++ src/index.js
@@ -163,10 +163,6 @@ class App extends React.Component {
               </p>
               <p className="instruction">downloads</p>
 
-              {/* <p className="instruction">
-              times from {this.state.startDate.toLocaleDateString()} to{' '}
-              {this.state.endDate.toLocaleDateString()}
-            </p> */}
             </div>
           ) : null}
         </div>
